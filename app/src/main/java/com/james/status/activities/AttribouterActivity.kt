18ea37b2cf65808bba6b566da9547081c94ba914
Andrew-Sor <133566800+Andrew-Sor@users.noreply.github.com>
2026-05-10 13:05:11 +0300

    Remove Attribouter dependencies - simplify AttribouterActivity

--- app/src/main/java/com/james/status/activities/AttribouterActivity.kt
+++ app/src/main/java/com/james/status/activities/AttribouterActivity.kt
@@ -16,65 +16,16 @@
 
 package com.james.status.activities
 
-import android.graphics.Color
-import android.graphics.PorterDuff
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
-import com.james.status.BuildConfig
 import com.james.status.R
 import com.james.status.Status
-import com.james.status.data.PreferenceData
-import me.jfenn.attribouter.Attribouter
-import me.jfenn.attribouter.fragments.AboutFragment
 
 class AttribouterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(Status.Theme.ACTIVITY_ATTRIBOUTER.getTheme(this))
-        setContentView(R.layout.activity_attribouter_about)
-
-        val color = if (PreferenceData.PREF_DARK_THEME.getValue(this)) Color.WHITE else Color.BLACK
-
-        val toolbar: Toolbar = findViewById<Toolbar>(R.id.toolbar).apply {
-            setTitleTextColor(color)
-        }
-
-        setSupportActionBar(toolbar)
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-
-            VectorDrawableCompat.create(resources, R.drawable.ic_attribouter_arrow_back, theme)?.let { drawable ->
-                drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
-                setHomeAsUpIndicator(drawable)
-            }
-        }
-
-        val bundle = Bundle().apply { putString(Attribouter.EXTRA_GITHUB_OAUTH_TOKEN, BuildConfig.GITHUB_TOKEN) }
-        val fragment = AboutFragment().apply { arguments = bundle }
-
-        if (savedInstanceState == null)
-            supportFragmentManager.beginTransaction().add(R.id.fragment, fragment).commit()
-        else
-            supportFragmentManager.beginTransaction().replace(R.id.fragment, fragment).commit()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home)
-            finish()
-
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(Bundle(), outPersistentState)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(Bundle())
+        setTheme(Status.Theme.ACTIVITY_NORMAL.getTheme(this))
+        setContentView(R.layout.activity_main)
     }
 }
